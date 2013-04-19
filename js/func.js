fc652e77c3527533bd213f9f768995348be4df79
Andrea Fortuna <andrea.fortuna@gmail.com>
2013-04-19 16:31:57 +0200

    Added gesture support for 'Mark as read' and 'Add a star'
    On post list, swipe left to mark post as read; swipe right to add a
    star.

--- js/func.js
+++ js/func.js
@@ -3,9 +3,9 @@ $("#MainPage").live('pageinit', global_pageinit);
 
 // Listen for orientation changes
 window.addEventListener("orientationchange", function() {
-	// Announce the new orientation number
-	alert(window.orientation);
-}, false);
+                        // Announce the new orientation number
+                        alert(window.orientation);
+                        }, false);
 
 
 function global_pageinit() {
@@ -16,8 +16,8 @@ function global_pageinit() {
 	var version = "0.0.3 Beta",
 	foothtml = "Version " + version,
 	cright = "&copy; 2013 Andrea Fortuna";
-
-
+    
+    
 	$( ".type-home .ui-content p.jqm-version" ).html( version );
 	$( ".footer-docs p.jqm-version" ).html( foothtml );
 	$( ".footer-docs p.af-copyright" ).html( cright );
@@ -35,100 +35,98 @@ function toast(message) {
 
 
 function timeConverter(UNIX_timestamp){
- var a = new Date(UNIX_timestamp*1000);
- var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-     var year = a.getFullYear();
-     var month = months[a.getMonth()];
-     var date = a.getDate();
-     var hour = a.getHours();
-     var min = a.getMinutes();
-     //var sec = a.getSeconds();
-     var time = date+','+month+' '+year+' '+hour+':'+min ;
-     return time;
- }
-
-
-
-   function caricaCategorie() {
-       
-       $.mobile.loading('show', {text:"Updating..."});
-       
-         sessionStorage.URL = localStorage.URL;
-    
-         sessionStorage.session_id=login(sessionStorage.URL,localStorage.Username,localStorage.Password);   
-    
-       if (sessionStorage.session_id=="" || sessionStorage.session_id==undefined) {
-            $.mobile.changePage("account.html", null, true, true);
-        }
-            
-       // var categorie =  getCategories(sessionStorage.URL, sessionStorage.session_id);
-                  
-       getCategoriesA(sessionStorage.URL, sessionStorage.session_id, function(categorie) {
-            var list = $('#categorieListView');
-           //Svuoto categorie
-           list.empty();
-           //list.append("<li data-role=\"list-divider\">Categories</li>");
-           list.append("<li data-theme='b'><a href='#'>Categories</a> <a href='#' onclick='caricaCategorie();'>refresh</a></li>");
-           //<a href='#' data-icon='refresh' data-iconpos='notext">Options</a>        
-              //list.listview("refresh");  
-             $.each(categorie, function() {
-                //if (this.unread > 0) 
-                    list.append("<li><a data-transition=\"slide\" href='feeds.html'  onclick=\"sessionStorage.catID='" + this.id + "';sessionStorage.catTitle='" + this.title + "'\">" + this.title + "</h4></a><span class=\"ui-li-count\">" + this.unread + "</span></li>");
-                        
-            });
-            list.listview("refresh");    
-            $.mobile.loading('hide');
-       });
-       
-       
-       
-       
-      //
-       
-        }
-
-function caricaFeeds() {
-        $.mobile.loading('show', {text:"Updating..."});
-        var list = $('#feedsListView');
-        list.empty();
-        getFeedsA(sessionStorage.URL, sessionStorage.session_id, sessionStorage.catID, function(feeds) {
-             $.each(feeds, function() {            
-                if (this.id != "")  list.append("<li><a data-transition=\"slide\" href='dettaglio.html'  onclick=\"sessionStorage.feedID='" + this.id + "';sessionStorage.feedTitle='" + this.title + "'\">" + this.title + "</h4></a><span class=\"ui-li-count\">" + this.unread + "</span></li>");                        
-             });
-            list.listview("refresh");
-            $.mobile.loading('hide');
-        });
+    var a = new Date(UNIX_timestamp*1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    //var sec = a.getSeconds();
+    var time = date+','+month+' '+year+' '+hour+':'+min ;
+    return time;
 }
 
 
 
-function caricaArticoli() {
-        var precedente=0;
-        var successivo=0;
+function caricaCategorie() {
     
-        $.mobile.loading('show', {text:"Updating..."});
-        $('#dettaglioTitoloFeed').text(sessionStorage.feedTitle);
-        var list = $('#articlesListView');
-        list.empty();
-        getArticlesA(sessionStorage.URL, sessionStorage.session_id, sessionStorage.feedID, function(articles) {
-            //alert(articles);
-            
-           
-           
+    $.mobile.loading('show', {text:"Updating..."});
+    
+    sessionStorage.URL = localStorage.URL;
+    
+    sessionStorage.session_id=login(sessionStorage.URL,localStorage.Username,localStorage.Password);
+    
+    if (sessionStorage.session_id=="" || sessionStorage.session_id==undefined) {
+        $.mobile.changePage("account.html", null, true, true);
+    }
+    
+    // var categorie =  getCategories(sessionStorage.URL, sessionStorage.session_id);
+    
+    getCategoriesA(sessionStorage.URL, sessionStorage.session_id, function(categorie) {
+                   var list = $('#categorieListView');
+                   //Svuoto categorie
+                   list.empty();
+                   //list.append("<li data-role=\"list-divider\">Categories</li>");
+                   list.append("<li data-theme='b'><a href='#'>Categories</a> <a href='#' onclick='caricaCategorie();'>refresh</a></li>");
+                   //<a href='#' data-icon='refresh' data-iconpos='notext">Options</a>
+                   //list.listview("refresh");
+                   $.each(categorie, function() {
+                          //if (this.unread > 0)
+                          list.append("<li><a data-transition=\"slide\" href='feeds.html'  onclick=\"sessionStorage.catID='" + this.id + "';sessionStorage.catTitle='" + this.title + "'\">" + this.title + "</h4></a><span class=\"ui-li-count\">" + this.unread + "</span></li>");
+                          
+                          });
+                   list.listview("refresh");
+                   $.mobile.loading('hide');
+                   });
+    
+    
+    
+    
+    //
+    
+}
 
-             $.each(articles, function() {   
-                sessionStorage.AllArticles = JSON.stringify(articles);
-                if (this.id != "") {
-                    //alert(this.id + "--" + nextArticle(this.id)); 
-                    list.append("<li><a data-transition=\"flip\" style='white-space : normal;" + ((this.unread === true) ?  "font-style:normal;": "font-size:0.8em;color:#888") + "' href='articolo.html'  onclick=\"sessionStorage.articleID='" + this.id + "'\">" + this.title + "<p style='margin:5px;'>" + timeConverter(this.updated) + "</p></a></li>");
-                    //list.append("<li><a data-transition=\"flip\" style='white-space : normal;" + ((this.unread === true) ?  "font-style:normal;": "font-size:0.8em;color:#888") + "' href='articolo.html'  onclick=\"sessionStorage.articleID='" + this.id + "';sessionStorage.NarticleID='" + nextArticle(this.id) + "'\">" + this.title + "<p style='margin:5px;'>" + timeConverter(this.updated) + "</p></a></li>");
-                    //list.append("<li><a data-transition=\"flip\" style='white-space : normal;" + ((this.unread === true) ?  "font-style:normal;": "font-size:0.8em;color:#888") + "' href='articolo.html'  onclick=\"sessionStorage.articleID='" + this.id + "';sessionStorage.AllArticles='" + nextArticle(this.id) + "'\">" + this.title + "<p style='margin:5px;'>" + timeConverter(this.updated) + "</p></a></li>");
-                }
-			
-             });
-            list.listview("refresh");
-            $.mobile.loading('hide');
-        });
+function caricaFeeds() {
+    $.mobile.loading('show', {text:"Updating..."});
+    var list = $('#feedsListView');
+    list.empty();
+    getFeedsA(sessionStorage.URL, sessionStorage.session_id, sessionStorage.catID, function(feeds) {
+              $.each(feeds, function() {
+                     if (this.id != "")  list.append("<li><a data-transition=\"slide\" href='dettaglio.html'  onclick=\"sessionStorage.feedID='" + this.id + "';sessionStorage.feedTitle='" + this.title + "'\">" + this.title + "</h4></a><span class=\"ui-li-count\">" + this.unread + "</span></li>");
+                     });
+              list.listview("refresh");
+              $.mobile.loading('hide');
+              });
+}
+
+
+
+function caricaArticoli(loader) {
+    var precedente=0;
+    var successivo=0;
+    
+    if (loader != "0") $.mobile.loading('show', {text:"Updating..."});
+    $('#dettaglioTitoloFeed').text(sessionStorage.feedTitle);
+    var list = $('#articlesListView');
+    list.empty();
+    getArticlesA(sessionStorage.URL, sessionStorage.session_id, sessionStorage.feedID, function(articles) {
+                 //alert(articles);
+                 
+                 
+                 
+                 
+                 $.each(articles, function() {
+                        sessionStorage.AllArticles = JSON.stringify(articles);
+                        if (this.id != "") {
+                        //alert(this.id + "--" + nextArticle(this.id));
+                        list.append("<li id='" + this.id + "'>  <a data-transition=\"flip\" style='white-space : normal;" + ((this.unread === true) ?  "font-style:normal;": "font-size:0.8em;color:#888") + "' href='articolo.html' onclick=\"sessionStorage.articleID='" + this.id + "'\">" + this.title + "<p style='margin:5px;'>" + timeConverter(this.updated) + "</p></a></li>");
+                                              }
+                        
+                        });
+                 list.listview("refresh");
+                 $.mobile.loading('hide');
+                 });
 }
 
 function caricaArticolo() {
